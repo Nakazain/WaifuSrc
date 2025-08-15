8d634d9741b1dbf438b98746d15580abf84918ba
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-15 22:58:08 +0700

    Fixing a bit

--- src/App.jsx
+++ src/App.jsx
@@ -20,14 +20,27 @@ export default function App() {
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <p className="text-4xl font-bold">Wife Search</p>
-        <div className="flex items-center">
-          <p>NSFW (18+)</p>
-          <Toggle />
+        <p className="text-4xl font-bold mt-8">Wife Search</p>
+        <div className="grid grid-cols-2 items-start justify-center w-full max-w-2xl p-4 mt-8">
+          <div className="flex items-center">
+            <p>NSFW (18+)</p>
+            <Toggle />
+          </div>
+          <div className="flex items-center">
+            <p className="mr-4">Tags 1</p>
+            <Tags />
+          </div>
+          <div className="flex items-center">
+            <p>GIF</p>
+            <Toggle />
+          </div>
+          <div className="flex items-center">
+            <p className="mr-4">Tags 2</p>
+            <Tags />
+          </div>
         </div>
-        <Tags />
         <Content url={data.url} art={data.artist} source={data.source} tag={data.tags?.[0]?.name} desc={data.tags?.[0]?.description} nsfw={data.is_nsfw} h={data.height} w={data.width} />
-        </div>
-      </>
-      )
+      </div>
+    </>
+  )
 }
