4e1ac41fcf7317a92e2e8da9ba4b8c6cca4162cf
Nakazain <naufalkaruniawanzain@gmail.com>
2025-08-18 22:03:35 +0700

    Make serverless function working properly (i hope)

--- api/download.js
+++ api/download.js
@@ -1,7 +1,9 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
   const imageUrl = req.query.url;
+
+  if (!imageUrl) {
+    return res.status(400).send("URL gambar diperlukan");
+  }
 
   try {
     const response = await fetch(imageUrl);
@@ -17,13 +19,14 @@ export default async function handler(req, res) {
     else if (contentType.includes("gif")) ext = "gif";
     else if (contentType.includes("webp")) ext = "webp";
 
-    const buffer = await response.arrayBuffer();
+    const buffer = Buffer.from(await response.arrayBuffer());
 
     res.setHeader("Content-Disposition", `attachment; filename=waifu.${ext}`);
     res.setHeader("Content-Type", contentType);
-    res.send(Buffer.from(buffer));
+    res.send(buffer);
 
   } catch (err) {
+    console.error("Error download:", err);
     res.status(500).send("Terjadi kesalahan saat mendownload");
   }
 }
